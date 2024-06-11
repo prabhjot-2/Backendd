b276341c0e235852ea3db5a4eda4423fae6a88d9
prabhjot-2 <prabh1146@gmail.com>
2024-06-12 00:28:47 +0530

    error handling in utils (apiresponse and apiError)

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,5 +1,27 @@
 class apiError extends Error {
-    // constructor(
-    //     statusCode,
-    //     message="something went wrong",
+    constructor(
+        statusCode,
+        message="something went wrong",
+        error=[],
+        stack=""
+    ){
+        super(message)// constructor overwrite kar reha enu messgae te karega hi karega
+
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success= false
+        this.errors=error
+
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+
+    }// e kita agar age ja k api errors ayi te is tra hi aange
+
 }
+
+
+export {apiError}
