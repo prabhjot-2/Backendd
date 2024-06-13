6c0cd99cf10bc934f79f0328031e3be7afbdbea4
prabhjot-2 <prabh1146@gmail.com>
2024-06-13 23:37:41 +0530

    method for upload a file in backend

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2  as cloudinary} from "cloudinary"
+
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloundinary= async (localFilePath)=>{
+    try {
+        if (!localFilePath) {
+            return null
+        }
+        //upload the file on cloudniary
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        //file has been successfully uploaded
+        console.log("file is uploaded successfully ", response.url);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove the locally saved temp file as the upload operaton got failed
+        return null;
+
+    }
+}
+
+export {uploadOnCloundinary}
